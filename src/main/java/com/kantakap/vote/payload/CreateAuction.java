efecb83fc842fddad2a9e3c73c1fa302ce8f774d
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-16 19:48:48 +0100

    Changed dependency from which the validation annotations were used

--- src/main/java/com/kantakap/vote/payload/CreateAuction.java
+++ src/main/java/com/kantakap/vote/payload/CreateAuction.java
@@ -1,11 +1,8 @@
 package com.kantakap.vote.payload;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
