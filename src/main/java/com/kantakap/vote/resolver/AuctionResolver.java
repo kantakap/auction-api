0271a20e0825bc1c72958b00b4ea13820cc46004
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-16 19:47:49 +0100

    Get User from Principal to set 'createdBy' field in the auction

--- src/main/java/com/kantakap/vote/resolver/AuctionResolver.java
+++ src/main/java/com/kantakap/vote/resolver/AuctionResolver.java
@@ -3,11 +3,12 @@ package com.kantakap.vote.resolver;
 import com.kantakap.vote.model.Auction;
 import com.kantakap.vote.payload.CreateAuction;
 import com.kantakap.vote.service.AuctionService;
+import com.kantakap.vote.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -18,10 +19,13 @@ import java.security.Principal;
 @Slf4j
 public class AuctionResolver {
     private final AuctionService auctionService;
+    private final UserService userService;
 
     @MutationMapping
 //    @PreAuthorize("hasRole('ROLE_USER')")
-    public Mono<Auction> createAuction(Principal principal, @Argument CreateAuction createAuction) {
-        return auctionService.createAuction(createAuction);
+    public Mono<Auction> createAuction(Principal principal, @Validated @Argument CreateAuction createAuction) {
+        return userService.me(principal)
+                .flatMap(user -> auctionService.createAuction(user, createAuction))
+                .log();
     }
 }
