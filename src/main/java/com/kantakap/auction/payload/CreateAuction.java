6d1cd75d275be32d7100e3af5fa412cdae31cfac
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-17 15:01:07 +0100

    Added @Builder to CreateAuction class

--- src/main/java/com/kantakap/auction/payload/CreateAuction.java
+++ src/main/java/com/kantakap/auction/payload/CreateAuction.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class CreateAuction {
     @NotNull(message = "Title must be specified")
     @NotBlank(message = "Title must be specified")
