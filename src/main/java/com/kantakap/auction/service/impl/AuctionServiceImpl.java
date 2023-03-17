36078f2dd66dc1424fc722f5bf221b113d593049
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-17 13:26:07 +0100

    Created AuctionValidator class to validate auction creation

--- src/main/java/com/kantakap/auction/service/impl/AuctionServiceImpl.java
+++ src/main/java/com/kantakap/auction/service/impl/AuctionServiceImpl.java
@@ -6,38 +6,41 @@ import com.kantakap.auction.model.User;
 import com.kantakap.auction.payload.CreateAuction;
 import com.kantakap.auction.repository.AuctionRepository;
 import com.kantakap.auction.service.AuctionService;
+import com.kantakap.auction.validator.AuctionValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class AuctionServiceImpl implements AuctionService {
     private final AuctionRepository auctionRepository;
 
+    /**
+     * Creates an auction.
+     * @param creator User who created the auction.
+     * @param createAuction Auction creation payload.
+     * @return Mono of the created auction.
+     */
     @Override
     public Mono<Auction> createAuction(User creator, CreateAuction createAuction) {
-        // validate start time
-        var now = LocalDateTime.now();
-        if (createAuction.getStartsAt().isBefore(now)) {
+        if (creator == null)
+            return Mono.error(new IllegalArgumentException("Creator cannot be null."));
+
+        if (!AuctionValidator.validateStartTimeIsBeforeNow(createAuction.getStartsAt()))
             return Mono.error(new IllegalArgumentException("Auction cannot start in the past."));
-        } else if (createAuction.getStartsAt().minusMinutes(30L).isBefore(now)) {
+
+        if (!AuctionValidator.validateStartTimeLessThanHalfHour(createAuction.getStartsAt()))
             return Mono.error(new IllegalArgumentException("Auction cannot start in less than 30 minutes."));
-        }
 
-        // validate maximum bid amount
-        if (createAuction.getMaximumBidAmount() > createAuction.getInitialBalance()) {
+        if (!AuctionValidator.validateMaximumBidAmountLessThanInitialBalance(createAuction.getMaximumBidAmount(), createAuction.getInitialBalance()))
             return Mono.error(new IllegalArgumentException("Maximum bid amount cannot be greater than initial balance."));
-        }
 
-        // validate funds loss prevention to be able to max bid with initial balance
-        var fundsLossPreventionPercentage = createAuction.getFundsLossPreventionPercentage() / 100;
-        var maxPossibleBidWithInitialBalance = createAuction.getInitialBalance() * fundsLossPreventionPercentage;
-        if (createAuction.getMaximumBidAmount() > maxPossibleBidWithInitialBalance) {
+        if (!AuctionValidator.canBidMaxAmount(
+                createAuction.getMaximumBidAmount(),
+                createAuction.getInitialBalance(),
+                createAuction.getFundsLossPreventionPercentage()))
             return Mono.error(new IllegalArgumentException("Maximum bid amount cannot be greater than initial balance * funds loss prevention percentage."));
-        }
 
         var auction = Auction.builder()
                 .createdBy(creator)
---
+++ src/main/java/com/kantakap/auction/validator/AuctionValidator.java
@@ -0,0 +1,53 @@
+package com.kantakap.auction.validator;
+
+import java.time.LocalDateTime;
+
+public class AuctionValidator {
+
+    /**
+     * Validate that the auction starts at a time in the future
+     * @param startsAt the time the auction starts
+     * @return true if the auction starts in the future, false otherwise
+     */
+    public static boolean validateStartTimeIsBeforeNow(LocalDateTime startsAt) {
+        var now = LocalDateTime.now();
+        return !startsAt.isBefore(now);
+    }
+
+    /**
+     * Validate that the auction starts at least 30 minutes from now
+     * @param startsAt the time the auction starts
+     * @return true if the auction starts at least 30 minutes from now, false otherwise
+     */
+    public static boolean validateStartTimeLessThanHalfHour(LocalDateTime startsAt) {
+        var now = LocalDateTime.now();
+        var halfHourFromNow = now.minusMinutes(30);
+        return !startsAt.isBefore(halfHourFromNow);
+    }
+
+    /**
+     * Validate that the maximum bid amount is less than the initial balance
+     * @param maximumBidAmount the maximum bid amount
+     * @param initialBalance the initial balance
+     * @return true if the maximum bid amount is less than the initial balance, false otherwise
+     */
+    public static boolean validateMaximumBidAmountLessThanInitialBalance(int maximumBidAmount, int initialBalance) {
+        return maximumBidAmount <= initialBalance;
+    }
+
+    /**
+     * Validate that the maximum bid amount is less than the initial balance * funds loss prevention percentage
+     * @param maximumBidAmount the maximum bid amount
+     * @param initialBalance the initial balance
+     * @param fundsLossPreventionPercentage the funds loss prevention percentage
+     * @return true if the maximum bid amount is less than the initial balance * funds loss prevention percentage, false otherwise
+     */
+    public static boolean canBidMaxAmount(
+            int maximumBidAmount,
+            int initialBalance,
+            int fundsLossPreventionPercentage
+    ) {
+        var maxPossibleBidWithInitialBalance = initialBalance * (fundsLossPreventionPercentage / 100);
+        return maximumBidAmount <= maxPossibleBidWithInitialBalance;
+    }
+}
