98a1b36b98a65412a8c3272d06e65fed2a2f0d3f
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-17 18:51:20 +0100

    Fixed auction saving test

--- src/test/java/com/kantakap/auction/unit/service/impl/AuctionServiceImplTest.java
+++ src/test/java/com/kantakap/auction/unit/service/impl/AuctionServiceImplTest.java
@@ -1,23 +1,34 @@
 package com.kantakap.auction.unit.service.impl;
 
 import com.kantakap.auction.model.Auction;
+import com.kantakap.auction.model.AuctionStatus;
 import com.kantakap.auction.model.User;
 import com.kantakap.auction.payload.CreateAuction;
+import com.kantakap.auction.repository.AuctionRepository;
 import com.kantakap.auction.service.impl.AuctionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.LocalDateTime;
+import java.util.function.Predicate;
 
-@ExtendWith(SpringExtension.class)
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class AuctionServiceImplTest {
+
+    @Mock
+    AuctionRepository auctionRepository;
 
     @InjectMocks
     AuctionServiceImpl auctionService;
@@ -27,7 +38,6 @@ class AuctionServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
         user = new User("Test user");
         createAuction = CreateAuction.builder()
                 .title("Test auction")
@@ -132,6 +142,40 @@ class AuctionServiceImplTest {
         StepVerifier.create(result)
                 .expectErrorMatches(throwable -> throwable.getMessage().equals("Maximum bid amount cannot be greater than initial balance * funds loss prevention percentage."))
                 .verify();
+    }
+
+    @Test
+    @DisplayName("Should create auction and save it when all parameters are valid.")
+    void shouldCreateAuctionObjectWhenAllParametersAreValid() {
+        // expected
+        ArgumentCaptor<Auction> auctionArgumentCaptor = ArgumentCaptor.forClass(Auction.class);
+
+        var expectedAuction = Auction.builder()
+                .title(createAuction.getTitle())
+                .startsAt(createAuction.getStartsAt())
+                .initialBalance(createAuction.getInitialBalance())
+                .fundsLossPreventionPercentage(createAuction.getFundsLossPreventionPercentage())
+                .initialTime(createAuction.getInitialTime())
+                .resetTime(createAuction.getResetTime())
+                .maximumBidAmount(createAuction.getMaximumBidAmount())
+                .minimumBidIncrement(createAuction.getMinimumBidIncrement())
+                .createdBy(user)
+                .status(AuctionStatus.CREATED)
+                .build();
+
+        when(auctionRepository.save(any(Auction.class))).thenReturn(Mono.just(expectedAuction));
+        var results = auctionService.createAuction(user, createAuction);
+
+        // then
+        verify(auctionRepository, times(1)).save(auctionArgumentCaptor.capture());
+
+        StepVerifier.create(results)
+                .expectNextMatches(actualAuction -> {
+                    assertThat(auctionArgumentCaptor.getValue()).usingRecursiveComparison().isEqualTo(expectedAuction);
+                    verify(auctionRepository, times(1)).save(auctionArgumentCaptor.capture());
+                    return true;
+                })
+                .verifyComplete();
     }
 
 }
