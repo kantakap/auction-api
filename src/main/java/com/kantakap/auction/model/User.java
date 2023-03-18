ff4faa9c4d2276bfdf4a9cf18de881c84c501da8
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-03-18 14:35:58 +0100

    Updated User entity to store osu ID

--- src/main/java/com/kantakap/auction/model/User.java
+++ src/main/java/com/kantakap/auction/model/User.java
@@ -18,12 +18,23 @@ public class User {
     @Id
     private String id;
     private String username;
+
+    private Long osuId;
     private Boolean active;
     private Set<GrantedAuthority> roles = new HashSet<>();
 
     @Builder
+    public User(String username, Long osuId) {
+        this.username = username;
+        this.osuId = osuId;
+        this.active = true;
+        this.roles.add(new SimpleGrantedAuthority(Role.ROLE_USER.toString()));
+    }
+
+    @Builder
     public User(String username) {
         this.username = username;
+        this.osuId = -1L;
         this.active = true;
         this.roles.add(new SimpleGrantedAuthority(Role.ROLE_USER.toString()));
     }
