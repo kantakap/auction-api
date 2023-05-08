fdb0813b43635f2290c2856ee99f7f58744c8799
Adam Orłowski <orlowski.adam.99@gmail.com>
2023-05-08 14:24:55 +0200

    Cleanup players data endpoint

--- src/main/java/com/kantakap/auction/controller/FileController.java
+++ src/main/java/com/kantakap/auction/controller/FileController.java
@@ -1,39 +1,22 @@
 package com.kantakap.auction.controller;
 
-import com.opencsv.CSVReader;
+import com.kantakap.auction.service.FileProcessorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class FileController {
+    private final FileProcessorService fileProcessorService;
 
     @PostMapping(value = "/auction/{auctionId}/players-data", consumes = "multipart/form-data")
 //    @Secured("ROLE_USER")
-    public List<List<String>> uploadPlayersData(@PathVariable String auctionId, @RequestPart("file") MultipartFile file) {
-        try {
-            Reader reader = new InputStreamReader(file.getInputStream());
-            List<List<String>> records = new ArrayList<>();
-            try (CSVReader csvReader = new CSVReader(reader)) {
-                String[] values = null;
-                while ((values = csvReader.readNext()) != null) {
-                    records.add(Arrays.asList(values));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return records;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public Flux<List<String>> uploadPlayersData(@PathVariable String auctionId, @RequestPart("file") FilePart file) {
+        return fileProcessorService.readCsvData(file);
     }
 }
